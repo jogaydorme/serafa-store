f9569009fad64f15d89553f1000d0a87d1bcdb9a
jogaydorme <drasomphis@gmail.com>
2025-05-06 16:04:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $('.menu-toggle').click(function () {
     $('.nav-links').toggleClass('active');
   });
+
+  let cartCount = 0;
+
+  $('.add-to-cart').click(function () {
+    cartCount++;
+    $('#cart-count').text(cartCount);
+    alert('Produto adicionado ao carrinho!');
+  });
 });
